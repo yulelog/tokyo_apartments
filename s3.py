71b400db806543a403dcb8699f561047900bd662
yulewang.io@gmail.com <yulelog@github.com>
2021-02-21 12:20:02 +0900

    add read_from_s3 function

--- s3.py
+++ s3.py
@@ -13,8 +13,14 @@ CLIENT = boto3.client(
 BUCKET = 'tokyo-apartments'
 
 def load_to_s3(df, filename):
+    """
+    Load dataframe in csv to s3, folder name is specified in filename
+    :param df: pandas dataframe
+    :param filename: filename including the folder name in the s3 bucket
+    :return : nothing
+    """
     csv_buffer = StringIO()
-    df.to_csv(csv_buffer)
+    df.to_csv(csv_buffer, index=False)
 
     CLIENT.put_object(
         Body = csv_buffer.getvalue(),
@@ -25,7 +31,7 @@ def load_to_s3(df, filename):
 
 def save_image(image_url, property_id):
     """
-    Given a url to a property's floorplan image, save the image down locally with property id
+    Given a url to a property's floorplan image, save the image to s3 bucket/floor_plans folder with property id
     :param image_url: string of the url to the image
     :param property_id: the string of property id
     :return : nothing
@@ -46,3 +52,15 @@ def save_image(image_url, property_id):
         )
     else:
         LOGGER.error(f"[ERROR] Unable to download the floorplan image of property: {property_id}")
+
+def read_from_s3(filename):
+    """
+    Read dataframe from csv file in s3
+    :param filename: filename including the folder name in the s3 bucket
+    :return : pandas dataframe
+    """
+    csv_obj = CLIENT.get_object(Bucket=BUCKET, Key=filename)
+    body = csv_obj['Body']
+    csv_string = body.read().decode('utf-8')
+    df = pd.read_csv(StringIO(csv_string))
+    return df
